5a819353b21a52a05d5dd9758bd663ca29bf4cd8
KMNK <kmnknmk+com-github@gmail.com>
2022-02-25 02:06:38 +0900

    Update

--- denops/@ddu-sources/git-log.ts
+++ denops/@ddu-sources/git-log.ts
@@ -20,25 +20,25 @@ export class Source extends BaseSource<Params> {
 
         controller.enqueue(lines.map((line, i) => {
           const matches = line.match(/^([*|\\\/ ]+) ([0-9a-z]+) (\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}) (.+)$/);
-          if (matches) {
-            const graph = matches[1];
-            const hash = matches[2];
-            const shortHash = hash.substr(0, 8);
-            const date = matches[3];
-            const info = matches[4];
-
-            return {
-              word: `${date} ${graph} ${shortHash} ${info}`,
-              hash: hash,
-            }
-          } else {
-            const graph = line.match(/^[*|\\\/ ]+$/)
+//          if (matches) {
+//            const graph = matches[1];
+//            const hash = matches[2];
+//            const shortHash = hash.substr(0, 8);
+//            const date = matches[3];
+//            const info = matches[4];
+//
+//            return {
+//              word: `${date} ${graph} ${shortHash} ${info}`,
+//              hash: hash,
+//            }
+//          } else {
+            const graph = line.match(/^[*|\\\/ ]+/)
 
             return {
               word: `${dateSpacer} ${graph}`,
               hash: "",
-            }
-          }
+            };
+//          }
         }));
 
         controller.close();
