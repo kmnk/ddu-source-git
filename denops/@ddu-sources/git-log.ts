25c6634430723a79f199be4773365fbfb0329cf9
KMNK <kmnknmk+com-github@gmail.com>
2022-02-25 02:22:24 +0900

    update

--- denops/@ddu-sources/git-log.ts
+++ denops/@ddu-sources/git-log.ts
@@ -12,8 +12,8 @@ export class Source extends BaseSource<Params> {
   gather(args: GatherArguments<Params>): ReadableStream<Item<ActionData>[]> {
     return new ReadableStream({
       async start(controller) {
-        const lines = await fn.systemlist(args.denops, "git log --graph --oneline");
-        //const lines = await fn.systemlist(args.denops, "git log --graph --oneline --date=format:'%Y-%m-%dT%H:%M:%S' --pretty=format:'%H %ad %an%d %s'");
+        //const lines = await fn.systemlist(args.denops, "git log --graph --oneline");
+        const lines = await fn.systemlist(args.denops, "git log --graph --oneline --date=format:'%Y-%m-%dT%H:%M:%S' --pretty=format:'%H %ad %an%d %s'");
 
         controller.enqueue(lines.map((line, i) => {
           return {
