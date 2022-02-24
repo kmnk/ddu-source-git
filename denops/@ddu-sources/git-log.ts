513dd351e070f8f74ff524b940900e45c71b306f
KMNK <kmnknmk+com-github@gmail.com>
2022-02-25 02:17:10 +0900

    updatee

--- denops/@ddu-sources/git-log.ts
+++ denops/@ddu-sources/git-log.ts
@@ -6,44 +6,39 @@ import { ActionData } from "../@ddu-kinds/git-log.ts"
 
 type Params = Record<never, never>;
 
-const date = "format:'%Y-%m-%dT%H:%M:%S'";
-const dateSpacer = "                   ";
-const pretty = "format:'%H %ad %an%d %s'";
-
 export class Source extends BaseSource<Params> {
   kind = "git-log";
 
   gather(args: GatherArguments<Params>): ReadableStream<Item<ActionData>[]> {
     return new ReadableStream({
       async start(controller) {
-        //const lines = await fn.systemlist(args.denops, `git log --graph --oneline --date=${date} --pretty=${pretty}`);
-        const lines = ['hoge', 'fuga'];
+        const date = "format:'%Y-%m-%dT%H:%M:%S'";
+        const dateSpacer = "                   ";
+        const pretty = "format:'%H %ad %an%d %s'";
+
+        const lines = await fn.systemlist(args.denops, `git log --graph --oneline --date=${date} --pretty=${pretty}`);
 
         controller.enqueue(lines.map((line, i) => {
-//          const matches = line.match(/^([*|\\\/ ]+) ([0-9a-z]+) (\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}) (.+)$/);
-//          if (matches) {
-//            const graph = matches[1];
-//            const hash = matches[2];
-//            const shortHash = hash.substr(0, 8);
-//            const date = matches[3];
-//            const info = matches[4];
-//
-//            return {
-//              word: `${date} ${graph} ${shortHash} ${info}`,
-//              hash: hash,
-//            }
-//          } else {
-            //const graph = line.match(/^[*|\\\/ ]+/);
+          const matches = line.match(/^([*|\\\/ ]+) ([0-9a-z]+) (\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}) (.+)$/);
+          if (matches) {
+            const graph = matches[1];
+            const hash = matches[2];
+            const shortHash = hash.substr(0, 8);
+            const date = matches[3];
+            const info = matches[4];
 
             return {
-              word: "test",
-              action: {
-                bufNr: "hoge",
-                lineNr: i + 1,
-              },
+              word: `${date} ${graph} ${shortHash} ${info}`,
+              hash: hash,
+            }
+          } else {
+            const graph = line.match(/^[*|\\\/ ]+$/)
+
+            return {
+              word: `${dateSpacer} ${graph}`,
               hash: "",
-            };
-//          }
+            }
+          }
         }));
 
         controller.close();
