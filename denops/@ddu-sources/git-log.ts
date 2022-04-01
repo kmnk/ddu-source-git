28cbcd9100c293db3d2660518161183f206523ab
KMNK <kmnknmk+com-github@gmail.com>
2022-04-01 22:32:21 +0900

    update

--- denops/@ddu-sources/git-log.ts
+++ denops/@ddu-sources/git-log.ts
@@ -1,7 +1,7 @@
 import * as fn from "https://deno.land/x/denops_std@v3.1.0/function/mod.ts";
 import type { GatherArguments } from "https://deno.land/x/ddu_vim@v0.14/base/source.ts";
-import type { Item } from "https://deno.land/x/ddu_vim@v0.14/types.ts";
-import { BaseSource } from "https://deno.land/x/ddu_vim@v0.14/types.ts";
+import type { Item } from "https://deno.land/x/ddu_vim@v1.4.0/types.ts";
+import { BaseSource } from "https://deno.land/x/ddu_vim@v1.4.0/types.ts";
 import { ActionData } from "../@ddu-kinds/git-log.ts"
 
 type Params = Record<never, never>;
@@ -16,7 +16,7 @@ export class Source extends BaseSource<Params> {
         //const lines = await fn.systemlist(args.denops, "git log --graph --oneline | cat");
         // こっちはだめ
         //const lines = await fn.systemlist(args.denops, "git log --graph --oneline --date=format:'%Y-%m-%dT%H:%M:%S' --pretty=format:'%H %ad %an%d %s' | cat");
-        // %d ないver
+        // %d ないver これもだめ
         const lines = await fn.systemlist(args.denops, "git log --graph --oneline --date=format:'%Y-%m-%dT%H:%M:%S' --pretty=format:'%H %ad %an %s' | cat");
 
         controller.enqueue(lines.map((line, i) => {
