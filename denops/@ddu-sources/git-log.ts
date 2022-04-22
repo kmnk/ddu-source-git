83e0b4124c3ce1f86b8e9e0a4d9c96269bcd1699
KMNK <kmnknmk+com-github@gmail.com>
2022-04-22 22:49:24 +0900

    update

--- denops/@ddu-sources/git-log.ts
+++ denops/@ddu-sources/git-log.ts
@@ -15,29 +15,29 @@ export class Source extends BaseSource<Params> {
         const lines = await fn.systemlist(args.denops, "git log --graph --oneline ---date=format:'%Y/%m/%d %H:%M:%S' --pretty=format:'%H %ad %an %s%d' | cat");
 
         controller.enqueue(lines.map((line, i) => {
-//          return {
-//              word: line,
-//          };
-          const matches = line.match(/^([*|\\\/ ]+) ([0-9a-z]+) (\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}) (.+)$/);
-          if (matches) {
-            const graph = matches[1];
-            const hash = matches[2];
-            const shortHash = hash.substr(0, 8);
-            const date = matches[3];
-            const info = matches[4];
+          return {
+              word: line,
+          };
+          //const matches = line.match(/^([*|\\\/ ]+) ([0-9a-z]+) (\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}) (.+)$/);
+          //if (matches) {
+          //  const graph = matches[1];
+          //  const hash = matches[2];
+          //  const shortHash = hash.substr(0, 8);
+          //  const date = matches[3];
+          //  const info = matches[4];
 
-            return {
-              word: `${date} ${graph} ${shortHash} ${info}`,
-              hash: hash,
-            }
-          } else {
-            const graph = line.match(/^[*|\\\/ ]+$/)
+          //  return {
+          //    word: `${date} ${graph} ${shortHash} ${info}`,
+          //    hash: hash,
+          //  }
+          //} else {
+          //  const graph = line.match(/^[*|\\\/ ]+$/)
 
-            return {
-              word: `                    ${graph}`,
-              hash: "",
-            }
-          }
+          //  return {
+          //    word: `                    ${graph}`,
+          //    hash: "",
+          //  }
+          //}
         }));
 
         controller.close();
