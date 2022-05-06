d932a48b7c3fd5fac5323abef26599c4ffe2a401
KMNK <kmnknmk+com-github@gmail.com>
2022-05-06 23:58:36 +0900

    update

--- denops/@ddu-kinds/git-log.ts
+++ denops/@ddu-kinds/git-log.ts
@@ -21,7 +21,7 @@ export class Kind extends BaseKind<Params> {
       for (const item of args.items) {
         const action = item?.action as ActionData;
         const hash = action.hash;
-        await fn.setreg(args.denops, '"', hash);
+        yank(args.denops, hash);
       }
 
       return Promise.resolve(ActionFlags.None);
@@ -33,24 +33,13 @@ export class Kind extends BaseKind<Params> {
   }
 }
 
-const paste = async(denops: Denops, item: DduItem, pasteKey: string) => {
-  const action = item?.action as ActionData;
-
-  if (action.text == null) {
-    return;
+const yank = async(denops: Denops, str: string) => {
+  await fn.setreg(args.denops, '"', hash);
+  if (await fn.has(args.denops, "clipboard")) {
+    await fn.setreg(
+      args.denops,
+      await args.denops.eval("v:register"),
+      hash,
+    );
   }
-  const regType = action.regType ? action.regType : "v";
-
-  const oldValue = fn.getreg(denops, '"');
-  const oldType = fn.getregtype(denops, '"');
-
-  await fn.setreg(denops, '"', action.text, regType);
-  try {
-    await denops.cmd('normal! ""' + pasteKey);
-  } finally {
-    await fn.setreg(denops, '"', oldValue, oldType);
-  }
-
-  // Open folds
-  await denops.cmd("normal! zv");
 };
