a14ce4a975cb1841fc06f4074c5b50aca55d25fc
KMNK <kmnknmk+com-github@gmail.com>
2022-05-15 19:59:27 +0900

    update

--- denops/@ddu-kinds/git-log.ts
+++ denops/@ddu-kinds/git-log.ts
@@ -25,6 +25,24 @@ export class Kind extends BaseKind<Params> {
       }
       return Promise.resolve(ActionFlags.None);
     },
+    revert: async(args) => {
+      const item = args.item[0];
+      const hash = action.hash;
+      await args.denops.cmd(`Git revert ${hash}`);
+      return Promise.resolve(ActionFlags.None);
+    },
+    reset: async(args) => {
+      const item = args.item[0];
+      const hash = action.hash;
+      await args.denops.cmd(`Git reset ${hash}`);
+      return Promise.resolve(ActionFlags.None);
+    },
+    reset_hard: async(args) => {
+      const item = args.item[0];
+      const hash = action.hash;
+      await args.denops.cmd(`Git reset ${hash}`);
+      return Promise.resolve(ActionFlags.None);
+    },
   };
 
   params(): Params {
